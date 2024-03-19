859b46f4c62f7286665bed52d81fcfb11fbe3937
rivenirvana <arvinverain@proton.me>
2024-03-19 12:25:13 +0800

    Use suda.vim

--- init.lua
+++ init.lua
@@ -228,6 +228,9 @@ vim.opt.rtp:prepend(lazypath)
 --
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
+  -- Workaround to sudo not working in-editor
+  -- https://github.com/neovim/neovim/issues/1716
+  'lambdalisue/suda.vim',
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
