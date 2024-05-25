657ebcd138818d87877ae22be36f60b85ce44052
rivenirvana <arvinverain@proton.me>
2024-05-25 16:31:36 +0800

    Reorganize remaps

--- lua/custom/plugins/remap.lua
+++ lua/custom/plugins/remap.lua
@@ -1,8 +1,10 @@
 vim.keymap.set('n', '<leader>xn', vim.cmd.Ex, { desc = 'E[X]ec [N]etrw' })
-vim.keymap.set('n', '<leader>xs', vim.cmd.so, { desc = 'E[X]ec [S]ource Code' })
+vim.keymap.set('n', '<leader>xs', vim.cmd.so, { desc = 'E[X]ec [S]ource script' })
 vim.keymap.set('n', '<leader>xc', '<cmd>!chmod +x %<CR>', { desc = 'E[X]ec [C]hmod +x', silent = true })
-vim.keymap.set('n', '<leader>xr', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = 'E[X]ec Sed [R]eplace' })
+vim.keymap.set('n', '<leader>xr', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = 'E[X]ec Search and [R]eplace' })
 vim.keymap.set('n', '<leader>xf', '<cmd>silent !tmux neww tmux-sessionizer<CR>', { desc = 'E[X]ec [F]ind Project' })
+
+vim.keymap.set('n', '<leader>F', vim.lsp.buf.format, { desc = 'vim.lsp.buf.format' })
 
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
@@ -25,7 +27,5 @@ vim.keymap.set({ 'n', 'v' }, '<leader>D', [["_d]])
 vim.keymap.set('x', '<leader>p', [["_dP]])
 
 vim.keymap.set('i', '<C-c>', '<Esc>')
-
-vim.keymap.set('n', '<leader>F', vim.lsp.buf.format, { desc = 'vim.lsp.buf.format' })
 
 return {}
