17686c90d749bc03425879dd92328a047b44a054
rivenirvana <arvinverain@proton.me>
2024-07-22 21:49:15 +0800

    Disable smartindent for now in favor of tpope/vim-sleuth

--- lua/custom/plugins/set.lua
+++ lua/custom/plugins/set.lua
@@ -1,6 +1,7 @@
-vim.opt.smartindent = true
-vim.opt.wrap = false
 vim.opt.colorcolumn = '80'
+vim.opt.wrap = false
+-- Probably unneeded if tpope/vim-sleuth works the same or better. Disabling for now.
+-- vim.opt.smartindent = true
 vim.opt.isfname:append { '@-@', '^' }
 
 return {}
