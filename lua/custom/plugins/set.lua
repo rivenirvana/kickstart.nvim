b2ea17d933c5971d5138afd536030b2274e6a9e1
rivenirvana <arvinverain@proton.me>
2024-07-23 04:53:34 +0800

    Fix :append()

--- lua/custom/plugins/set.lua
+++ lua/custom/plugins/set.lua
@@ -2,6 +2,6 @@ vim.opt.colorcolumn = '80'
 vim.opt.wrap = false
 -- Probably unneeded if tpope/vim-sleuth works the same or better. Disabling for now.
 -- vim.opt.smartindent = true
-vim.opt.isfname:append { '@-@', '^' }
+vim.opt.isfname:append('@-@', '^')
 
 return {}
