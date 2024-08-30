4d84f038c834d1ab9c168c6a23c23b277147a772
Arvin Verain <arvinverain@proton.me>
2024-08-30 21:56:56 +0800

    Add a duplicate but more contextual keybind for code diagnostic

--- init.lua
+++ init.lua
@@ -562,6 +562,10 @@ require('lazy').setup({
           -- or a suggestion from your LSP for this to activate.
           map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
 
+          -- Open code diagnostic in a float window.
+          -- Alternatively, nvim 0.10 added <C-W>d as a default keybind for this function.
+          map('<leader>cd', vim.diagnostic.open_float, '[C]ode [D]iagnostic')
+
           -- WARN: This is not Goto Definition, this is Goto Declaration.
           --  For example, in C this would take you to the header.
           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
