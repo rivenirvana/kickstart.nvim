7dd39ae53d5d355c129e2bd1c4ac360c877693e4
Arvin Verain <arvinverain@proton.me>
2024-09-12 08:14:02 +0800

    Update config

--- init.lua
+++ init.lua
@@ -856,7 +856,7 @@ require('lazy').setup({
     'folke/tokyonight.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     -- opts = {
-    --   transparent = false,
+    --   transparent = true,
     --   styles = {
     --     sidebars = 'transparent',
     --     floats = 'transparent',
