59bdf3153baeea4c0cf0d6b1ed76fbe0a10f7637
Arvin Verain <arvinverain@proton.me>
2024-09-23 14:25:16 +0800

    Reorder kitty config sequence

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -9,7 +9,7 @@ vim.api.nvim_create_autocmd({ 'VimEnter', 'VimResume' }, {
 vim.api.nvim_create_autocmd({ 'VimLeave', 'VimSuspend' }, {
   group = vim.api.nvim_create_augroup('KittyUnsetVarVimLeave', { clear = true }),
   callback = function()
-    io.stdout:write '\x1b]30101\x1b\\'
     io.stdout:write '\x1b]1337;SetUserVar=KITTY_IN_NVIM\007'
+    io.stdout:write '\x1b]30101\x1b\\'
   end,
 })
