5f2e34e4764dc1ee8e45cf6cddaeda539c321209
Arvin Verain <arvinverain@proton.me>
2024-09-29 04:23:14 +0800

    Disable transparent theme background

--- init.lua
+++ init.lua
@@ -861,7 +861,7 @@ require('lazy').setup({
     'folke/tokyonight.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     opts = {
-      transparent = true,
+      -- transparent = true,
       styles = {
         sidebars = 'transparent',
         floats = 'transparent',
