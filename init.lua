c3e8c8a44ba9108c96d849b7f29b0ab8db5c8870
Arvin Verain <arvinverain@proton.me>
2024-10-13 02:13:24 +0800

    Use ruff

--- init.lua
+++ init.lua
@@ -728,10 +728,17 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        python = { 'isort', 'black' },
+        python = { 'ruff' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         javascript = { 'prettierd', 'prettier', stop_after_first = true },
+      },
+      formatters = {
+        ruff = {
+          command = 'ruff',
+          args = { 'format', '-' },
+          stdin = true,
+        },
       },
     },
   },
