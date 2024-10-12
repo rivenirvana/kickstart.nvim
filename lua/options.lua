3ccd6bbe60269a256eb9309e580c85b98f28d8a5
Arvin Verain <arvinverain@proton.me>
2024-10-13 02:14:42 +0800

    Update options.lua

--- lua/options.lua
+++ lua/options.lua
@@ -2,4 +2,5 @@ vim.opt.colorcolumn = '80'
 vim.opt.wrap = false
 -- Probably unneeded if tpope/vim-sleuth works the same or better. Disabling for now.
 -- vim.opt.smartindent = true
-vim.opt.isfname:append('@-@', '^')
+vim.opt.isfname:append '@-@,^'
+vim.opt.iskeyword:remove '_'
