3555f56c0488627e7054138108b3533eb8d56e96
Arvin Verain <arvinverain@proton.me>
2024-12-30 02:24:47 +0800

    Specify tab behavior

--- lua/options.lua
+++ lua/options.lua
@@ -4,3 +4,7 @@ vim.opt.wrap = false
 -- vim.opt.smartindent = true
 vim.opt.isfname:append '@-@,^'
 vim.opt.iskeyword:remove '_'
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
