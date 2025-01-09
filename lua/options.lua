f07224995e7cc48fd173a3e1ad65341a486b9a86
Arvin Verain <arvinverain@proton.me>
2025-01-09 12:41:25 +0800

    build: Fix broken right-click

--- lua/options.lua
+++ lua/options.lua
@@ -2,7 +2,7 @@ vim.opt.colorcolumn = '80'
 vim.opt.wrap = false
 -- Probably unneeded if tpope/vim-sleuth works the same or better. Disabling for now.
 -- vim.opt.smartindent = true
-vim.opt.isfname:append '@-@,^'
+vim.opt.isfname:append '@-@'
 vim.opt.iskeyword:remove '_'
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
