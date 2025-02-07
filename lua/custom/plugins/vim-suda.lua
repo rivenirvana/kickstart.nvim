645f7049647b3a741eead23c97f7c43f16e5623a
Arvin Verain <arvinverain@proton.me>
2025-02-07 14:06:09 +0800

    fix: Use new suda repo name

--- lua/custom/plugins/vim-suda.lua
+++ lua/custom/plugins/vim-suda.lua
@@ -1,7 +1,9 @@
-vim.g.suda_smart_edit = 1
+if not vim.o.diff then
+  vim.g.suda_smart_edit = 1
+end
 
 return {
   -- Workaround to sudo not working in-editor
   -- https://github.com/neovim/neovim/issues/1716
-  'lambdalisue/suda.vim',
+  'lambdalisue/vim-suda',
 }
