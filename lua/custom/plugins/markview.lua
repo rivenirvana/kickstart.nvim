38a472278b1a6b7e4f606915d6b465683b6342c5
Arvin Verain <arvinverain@proton.me>
2025-02-07 14:06:49 +0800

    fix: Use new basic setup

--- lua/custom/plugins/markview.lua
+++ lua/custom/plugins/markview.lua
@@ -1,12 +1,4 @@
 return {
   'OXY2DEV/markview.nvim',
-  lazy = false, -- Recommended
-  -- ft = 'markdown', -- If you decide to lazy-load anyway
-
-  opts = {},
-
-  dependencies = {
-    'nvim-treesitter/nvim-treesitter',
-    'nvim-tree/nvim-web-devicons',
-  },
+  lazy = false,
 }
