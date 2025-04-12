f80541c088361da0eccaedf04e8810913c1d8d99
Arvin Verain <arvinverain@proton.me>
2025-04-13 07:50:59 +0800

    Treat underscore as keyword again for treesitter compat

--- lua/options.lua
+++ lua/options.lua
@@ -3,7 +3,6 @@ vim.opt.wrap = false
 -- Probably unneeded if tpope/vim-sleuth works the same or better. Disabling for now.
 -- vim.opt.smartindent = true
 vim.opt.isfname:append '@-@'
-vim.opt.iskeyword:remove '_'
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
